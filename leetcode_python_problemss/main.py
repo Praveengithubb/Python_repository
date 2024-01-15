92c7e85534c82d6b184583239c23b97b3f8613aa
Praveengithubb <praveenraj2670@gmail.com>
2024-01-16 00:13:15 +0530

    Initial commit

---
+++ leetcode_python_problemss/main.py
@@ -0,0 +1,12 @@
+from leetcode_python_problemss import find_the_difference_of_two_array
+
+
+def main():
+    nums1 = [1, 2, 3]
+    nums2 = [2, 4, 6]
+    result = find_the_difference_of_two_array.find_difference(nums1, nums2)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
