4f0b024db8faa1b5d8dc0673d8693251c9248e2e
Praveengithubb <praveenraj2670@gmail.com>
2024-02-21 02:39:36 +0530

    initial commit

---
+++ leetcode_problems/Letter_combination_of_a_phone_number.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class LetterCombination:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        phone_map = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        output = []
+
+        def backtrack(combination, next_digit):
+            if not next_digit:
+                output.append(combination)
+                return
+            else:
+                letters = phone_map[int(next_digit[0]) - 2]
+                for letter in letters:
+                    backtrack(combination + letter, next_digit[1:])
+        backtrack("", digits)
+        return output
+
+
+l = LetterCombination()
+digits = "23"
+print(l.letterCombinations(digits))
