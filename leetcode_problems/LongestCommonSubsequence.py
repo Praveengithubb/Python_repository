a9e85e396fcbc06c5638d0e52ccb298b7881f30c
Praveengithubb <praveenraj2670@gmail.com>
2024-02-28 02:44:17 +0530

    initial commit

---
+++ leetcode_problems/LongestCommonSubsequence.py
@@ -0,0 +1,18 @@
+class LongestCommonSubsequence:
+    def longest_common_subsequence(self, text1, text2):
+        length1 = len(text1)
+        length2 = len(text2)
+        dp = [[0] * (length2 + 1) for _ in range(length1 + 1)]
+        for i in range(1, length1 + 1):
+            for j in range(1, length2 + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[length1][length2]
+
+
+l = LongestCommonSubsequence()
+text1 = "abc"
+text2 = "abc"
+print(l.longest_common_subsequence(text1, text2))
